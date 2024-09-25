3694d06f4f384715732a35e05f64bb7bc7f6e741
leehyunsuck <leehyunsuck01@gmail.com>
2024-09-25 14:58:00 +0900

    Separation of email transfer service classes

--- src/main/java/Shingu/Interviewer/basic/updatePassword/EmailSendUpdatePassword.java
+++ src/main/java/Shingu/Interviewer/basic/updatePassword/EmailSendUpdatePassword.java
@@ -1,5 +1,6 @@
 package Shingu.Interviewer.basic.updatePassword;
 
+import Shingu.Interviewer.servic.SendMailService;
 import Shingu.Interviewer.servic.UserInfoService;
 import jakarta.mail.*;
 import jakarta.mail.internet.InternetAddress;
@@ -32,9 +33,11 @@ public class EmailSendUpdatePassword extends HttpServlet {
     private static final Pattern pattern = Pattern.compile(EMAIL_REGEX);
 
     private final UserInfoService userInfoService;
+    private final SendMailService sendMailService;
 
-    public EmailSendUpdatePassword(UserInfoService userInfoService) {
+    public EmailSendUpdatePassword(UserInfoService userInfoService, SendMailService sendMailService) {
         this.userInfoService = userInfoService;
+        this.sendMailService = sendMailService;
     }
 
     public static boolean isValidEmail(String email) {
@@ -68,43 +71,18 @@ public class EmailSendUpdatePassword extends HttpServlet {
         Random random = new Random();
         int code = random.nextInt(900000) + 100000;
 
-        //이메일 전송 구현
-        try {
-            // 메일 서버 설정
-            Properties properties = new Properties();
-            properties.put("mail.smtp.host", "smtp.gmail.com");
-            properties.put("mail.smtp.port", "587");
-            properties.put("mail.smtp.auth", "true");
-            properties.put("mail.smtp.starttls.enable", "true");
+        String title = "Interviewer 비밀번호 변경 인증코드 전송";
+        String htmlContent =
+                """
+                <h1> Interviewer 인증 코드 </h1>
+                <p style="font-size:12px; color:red;"> 해당 코드는 비밀번호 변경을 위한 정보이므로 타인에게 알려주지 마세요. </p>
+                <p> [인증코드] </p>
+                """.replace("[인증코드]", String.valueOf(code));
 
-            // Gmail 인증
-            Authenticator auth = new Authenticator() {
-                protected PasswordAuthentication getPasswordAuthentication() {
-                    return new PasswordAuthentication(host, mailPassword);
-                }
-            };
+        if (sendMailService.sendMail(email, title, htmlContent)) {
 
-            // 세션 생성
-            Session session = Session.getInstance(properties, auth);
-
-            // 이메일 생성
-            Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(host, "취업완 모의면접 인증코드 발신용"));  // 발신자 설정
-            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));  // 수신자 설정
-            message.setSubject("취업완 비밀번호 수정 인증코드를 전송해드립니다.");  // 제목 설정
-
-            // HTML 내용 생성
-            String htmlContent = "<h1>취업완 (모의면접) 인증 코드</h1>"
-                    + "<p>귀하의 인증 코드는 다음과 같습니다:</p>"
-                    + "<p style=\"font-size:18px; color:blue;\">" + code + "</p>"
-                    + "<p style=\"font-size:12px; color:red;\"> 해당 코드는 회원가입을 위한 정보이므로 타인에게 알려주지 마세요.";
-
-            message.setContent(htmlContent, "text/html; charset=utf-8");
-
-            // 이메일 전송
-            Transport.send(message);
-        } catch (MessagingException e) {
-            throw new RuntimeException(e);
+        } else {
+            //error
         }
 
         req.getSession().setAttribute("email", email);
