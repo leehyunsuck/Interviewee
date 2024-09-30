e35b4e65312fd1d56ccb7d8ff6972b9c8588d8e0
leehyunsuck <leehyunsuck01@gmail.com>
2024-09-30 23:14:00 +0900

    - error 페이지 코멘트만 다르므로 error.html로 통합 - error 페이지 사용시 model에 errorMsg로 코멘트와 hrefValue로 이동페이지 넘기면 됩니다

--- src/main/java/Shingu/Interviewer/servic/LoginService.java
+++ src/main/java/Shingu/Interviewer/servic/LoginService.java
@@ -17,14 +17,16 @@ public class LoginService {
         // 가입 여부 확인
         if (!userInfoService.isEmailExists(email)) {
             model.addAttribute("errorMsg", "가입되지 않은 이메일 입니다.");
-            return "error_email";
+            model.addAttribute("hrefValue", "login");
+            return "error";
         }
 
         // 로그인 정보 일치 확인
         String hashPassword = HashEncode.encode(email, password);
         if (!userInfoService.isAccountExists(email, hashPassword)) {
             model.addAttribute("errorMsg", "옳바르지 않은 비밀번호 입니다.");
-            return "error_pw";
+            model.addAttribute("hrefValue", "login");
+            return "error";
         }
 
         session.setAttribute("loggedInEmail", email);
