6471279475fc3804c306aa3264c567abe23b6fb0
leehyunsuck <leehyunsuck01@gmail.com>
2024-10-02 14:57:37 +0900

    - 회원가입 기능 활성화 - 직군 선택 더미파일 생성

--- src/main/java/Shingu/Interviewer/servic/RegisterService.java
+++ src/main/java/Shingu/Interviewer/servic/RegisterService.java
@@ -35,52 +35,72 @@ public class RegisterService {
     }
 
     public String register(String email, String password, String checkPassword, Model model) {
-        // 규칙확인 로직
-        if (!password.equals(checkPassword)) {
-            model.addAttribute("error", "비밀번호가 일치하지 않습니다.");
-            return "register";
-        }
-
-        // 규칙 다 지켜졌으면 DB에 정보 저장
-        String hashPassword = HashEncode.encode(email, password);
-
-        UserInfo userInfo = new UserInfo();
-        userInfo.setEmail(email);
-        userInfo.setPassword(hashPassword);
-        userInfoService.saveUser(userInfo);
-        model.addAttribute("loggedInEmail", email);
-        return "success_register";
-    }
-
-    public  String registerSendMail(Model model, String email) {
+        model.addAttribute("email", email);
+        model.addAttribute("action", "register");
         //이메일 유형 확인 로직
         if (!isValidEmail(email) || email.length() > 320) {
-            model.addAttribute("email", email);
-            model.addAttribute("invalidEmail", true);
-            return "register";
+            model.addAttribute("errorMsg", "");
+        } else if (userInfoService.isEmailExists(email)) {
+            model.addAttribute("errorMsg", "이미 가입된 이메일 입니다.");
+        } else if (!password.equals(checkPassword)) {
+            model.addAttribute("errorMsg", "비밀번호가 일치하지 않습니다.");
+        } else if (password.length() < 8 || password.length() > 16) {
+            model.addAttribute("errorMsg", "비밀번호는 8자 이상 16자 이하로 입력해주세요.");
+        } else {
+            // 규칙 다 지켜졌으면 DB에 정보 저장
+            String hashPassword = HashEncode.encode(email, password);
+
+            UserInfo userInfo = new UserInfo();
+            userInfo.setEmail(email);
+            userInfo.setPassword(hashPassword);
+            userInfoService.saveUser(userInfo);
+
+            model.addAttribute("loggedInEmail", email);
+            return "success_register";
+        }
+        return "register";
+    }
+
+    public  String sendMail(Model model, String email) {
+        //이메일 유형 확인 로직
+        if (!isValidEmail(email) || email.length() > 320) {
+            model.addAttribute("errorMsg", "유효한 이메일 형식이 아닙니다");
+            model.addAttribute("hrefValue", "register");
+            return "error";
         }
 
         //이미 가입한 이메일 확인 로직
         if (userInfoService.isEmailExists(email)) {
-            model.addAttribute("email", email);
-            model.addAttribute("alreadyEmail", true);
-            return "register";
+            model.addAttribute("errorMsg", "이미 가입된 이메일 입니다.");
+            model.addAttribute("hrefValue", "register");
+            return "error";
         }
+        model.addAttribute("action", "checkCode");
+        model.addAttribute("email", email);
 
-        //위 두 조건 다 통과면 인증번호 전송
         Random random = new Random();
         int code = random.nextInt(900000) + 100000;
-        //인증번호는 service/SendMailService 에 구현해놨으니 호출해서 쓰면 됨
+
         String content = "귀하의 인증번호는 " + code + "입니다."; // 이메일 내용
         sendMailService.sendMail(email, "이메일 인증", content); // 메일 전송
-        model.addAttribute("code", code);
-        model.addAttribute("email", email);
+        model.addAttribute("action", "checkCode");
         verificationCodes.put(email, code);
-        return "register";
 
+        return "register";
     }
-    public boolean verifyCode(String email, int code) {
-        Integer storedCode = verificationCodes.get(email);
-        return storedCode != null && storedCode.equals(code);
+    public String checkCode(String email, String code, Model model) {
+        model.addAttribute("email", email);
+
+        String storedCode = String.valueOf(verificationCodes.get(email));
+
+        if (storedCode.equals(code)) {
+            model.addAttribute("errorMsg", "");
+            model.addAttribute("action", "register");
+        } else {
+            model.addAttribute("errorMsg", "인증번호가 일치하지 않습니다.");
+            model.addAttribute("action", "checkCode");
+        }
+
+        return "register";
     }
 }
